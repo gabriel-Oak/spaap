674afec658813009c951eafd63d8349263faa9f1
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2018-12-21 20:23:40 -0200

    Updates

---
+++ precache-manifest.c8e6227b3d07cf0bbb6e385aadf53dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "813597f5d88867fdd09c",
+    "url": "/spaap/static/js/runtime~main.813597f5.js"
+  },
+  {
+    "revision": "3eaa2f522d03107049b6",
+    "url": "/spaap/static/js/main.3eaa2f52.chunk.js"
+  },
+  {
+    "revision": "a03cc6e38d20d9876f11",
+    "url": "/spaap/static/js/1.a03cc6e3.chunk.js"
+  },
+  {
+    "revision": "3eaa2f522d03107049b6",
+    "url": "/spaap/static/css/main.fa8bc454.chunk.css"
+  },
+  {
+    "revision": "a03cc6e38d20d9876f11",
+    "url": "/spaap/static/css/1.b930b71f.chunk.css"
+  },
+  {
+    "revision": "ff29dbac7f27d0d69c5fcd302257a3a4",
+    "url": "/spaap/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc40743fd38d80a3316b7305eebfdc16.js"
+  "/spaap/precache-manifest.c8e6227b3d07cf0bbb6e385aadf53dad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/spaap/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
